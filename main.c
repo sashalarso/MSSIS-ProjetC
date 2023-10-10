1e967081fe6542ab151db8278d6bffec5aa15e5c
sashalarso <sasha.larsonneur@yahoo.fr>
2023-10-10 11:57:39 +0200

    test

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include<stdlib.h>
 #include <string.h>
 #include <openssl/md5.h>
+#include <openssl/sha.h>
 
 typedef struct {
     char* key;
@@ -38,6 +39,21 @@ void md5_hash_from_string (char *string, char *hash)
     }
 }
 
+void sha256_hash_string(const char *string, char outputBuffer[65]) {
+    unsigned char hash[SHA256_DIGEST_LENGTH];
+    SHA256_CTX sha256;
+
+    SHA256_Init(&sha256);
+    SHA256_Update(&sha256, string, strlen(string));
+    SHA256_Final(hash, &sha256);
+
+    for (int i = 0; i < SHA256_DIGEST_LENGTH; i++) {
+        sprintf(outputBuffer + (i * 2), "%02x", hash[i]);
+    }
+
+    outputBuffer[64] = 0;
+}
+
 int main(void) {
     int num_items = 0;
     item items[] = {};
@@ -50,10 +66,19 @@ int main(void) {
     }
     printf("linear_search: value of 'bob' is %d\n", found->value);
     //printf("%d", items[0].value);
-    char string[255] = "Hello World";
+    char string[255] = "Hello Worlde";
     char md5_hash[2*MD5_DIGEST_LENGTH+1] = "";
     md5_hash_from_string(string, md5_hash);
     printf("%s\n", md5_hash);
+
+
+    const char *input_string = "Votre_chaine_a_hasher";
+    char output_hash[65];
+
+    sha256_hash_string(input_string, output_hash);
+
+    printf("Chaîne originale : %s\n", input_string);
+    printf("Hachage SHA-256 : %s\n", output_hash);
    
     return 0;
 }
---
+++ test.c
@@ -0,0 +1,167 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <openssl/evp.h>
+
+// Fonction pour générer un hachage SHA-256
+void sha256_hash_string(const char *string, unsigned char *output) {
+    EVP_MD_CTX *mdctx;
+    const EVP_MD *md;
+
+    OpenSSL_add_all_digests();
+    md = EVP_get_digestbyname("sha256");
+    mdctx = EVP_MD_CTX_new();
+    EVP_DigestInit_ex(mdctx, md, NULL);
+    EVP_DigestUpdate(mdctx, string, strlen(string));
+    EVP_DigestFinal_ex(mdctx, output, NULL);
+    EVP_MD_CTX_free(mdctx);
+    EVP_cleanup();
+}
+
+// Fonction pour charger la table de correspondance chaîne-condensat (T3C) depuis un fichier
+int load_table_from_file(const char *filename, char ***table, int *num_entries) {
+    FILE *file = fopen(filename, "r");
+    if (!file) {
+        perror("Erreur lors de l'ouverture du fichier T3C");
+        return 0;
+    }
+
+    char line[256];
+    int max_entries = 100; // Un nombre arbitraire initial de lignes
+    *table = (char **)malloc(max_entries * sizeof(char *));
+    if (*table == NULL) {
+        perror("Erreur lors de l'allocation de mémoire");
+        fclose(file);
+        return 0;
+    }
+
+    *num_entries = 0;
+    while (fgets(line, sizeof(line), file)) {
+        line[strcspn(line, "\n")] = '\0';
+        (*table)[(*num_entries)++] = strdup(line);
+
+        if (*num_entries >= max_entries) {
+            // Réallouer la mémoire pour plus de lignes si nécessaire
+            max_entries *= 2;
+            *table = (char **)realloc(*table, max_entries * sizeof(char *));
+            if (*table == NULL) {
+                perror("Erreur lors de la réallocation de mémoire");
+                fclose(file);
+                return 0;
+            }
+        }
+    }
+
+    fclose(file);
+    return 1;
+}
+
+// Fonction pour rechercher une chaîne parmi les condensats dans la table T3C
+int lookup_in_table(const char *target_hash, char **table, int num_entries, char **found_strings) {
+    int found = 0;
+
+    for (int i = 0; i < num_entries; i++) {
+        char *line = table[i];
+        char *chaine = strtok(line, " -> ");
+        char *condensat = strtok(NULL, " -> ");
+
+        
+
+        if (condensat != NULL && chaine != NULL && strcmp(condensat, target_hash) == 0) {
+            
+            found = 1;
+            found_strings[i] = strdup(chaine);
+            printf("%s",found_strings[i]);
+        }
+    }
+
+    return found;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 4 && argc != 2) {
+        fprintf(stderr, "Utilisation : %s G <fichier_dictionnaire> <fichier_T3C> OU %s L <fichier_T3C>\n", argv[0], argv[0]);
+        return 1;
+    }
+
+    if (argc == 4 && strcmp(argv[1], "G") == 0) {
+        // Mode G (génération de condensats)
+        const char *input_file = argv[2];
+        const char *output_file = argv[3];
+
+        FILE *input = fopen(input_file, "r");
+        if (input == NULL) {
+            perror("Erreur lors de l'ouverture du fichier dictionnaire");
+            return 1;
+        }
+
+        FILE *output = fopen(output_file, "w");
+        if (output == NULL) {
+            perror("Erreur lors de l'ouverture du fichier de sortie T3C");
+            fclose(input);
+            return 1;
+        }
+
+        char line[256];
+        while (fgets(line, sizeof(line), input)) {
+            line[strcspn(line, "\n")] = '\0'; // Supprimer le caractère de saut de ligne
+            unsigned char sha256_hash[EVP_MAX_MD_SIZE]; // Utilisation de EVP_MAX_MD_SIZE
+
+            sha256_hash_string(line, sha256_hash);
+
+            fprintf(output, "%s -> ", line);
+            for (int i = 0; i < EVP_MD_size(EVP_get_digestbyname("sha256")); i++) {
+                fprintf(output, "%02x", sha256_hash[i]);
+            }
+            fprintf(output, "\n");
+        }
+
+        fclose(input);
+        fclose(output);
+
+        printf("Génération de condensats terminée. Les résultats ont été écrits dans %s.\n", output_file);
+    } else if (argc == 2 && strcmp(argv[1], "L") == 0) {
+        // Mode L (recherche parmi une liste de condensats)
+        char **table = NULL;
+        int num_entries = 0;
+
+        if (!load_table_from_file("table_de_correspondance.txt", &table, &num_entries)) {
+            fprintf(stderr, "Erreur lors du chargement de la table T3C.\n");
+            return 1;
+        }
+
+        char target_hash[65]; // Pour stocker le condensat cible
+        printf("Entrez le condensat à rechercher (en hexadécimal) : ");
+        if (scanf("%64s", target_hash) != 1) {
+            fprintf(stderr, "Erreur lors de la saisie du condensat.\n");
+            return 1;
+        }
+
+        char *found_strings[num_entries];
+        int found = lookup_in_table(target_hash, table, num_entries, found_strings);
+
+        if (found) {
+            printf("Chaîne(s) correspondante(s) trouvée(s) :\n");
+            printf("%s\n", found_strings[0]);
+            for (int i = 0; i < num_entries; i++) {
+                if (found_strings[i] != NULL) {
+                    printf("%s\n", found_strings[i]);
+                    free(found_strings[i]);
+                }
+            }
+        } else {
+            printf("Aucune correspondance trouvée pour le condensat spécifié.\n");
+        }
+
+        // Libérer la mémoire allouée
+        for (int i = 0; i < num_entries; i++) {
+            free(table[i]);
+        }
+        free(table);
+    } else {
+        fprintf(stderr, "Mode invalide. Utilisation : G (generate) OU L (lookup)\n");
+        return 1;
+    }
+
+    return 0;
+}
