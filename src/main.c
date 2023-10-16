aa9d0f400017aa88aed626c76f3b67bee18b24f2
sashalarso <85192589+sashalarso@users.noreply.github.com>
2023-10-16 17:47:52 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -2,13 +2,11 @@
 #include <stdlib.h>
 #include <string.h>
 #include <openssl/evp.h>
+#include "table.h"
 
 // Fonction pour générer un hachage SHA-256
 
-typedef struct {
-    char *hash;    // Le condensat (hash)
-    char *chaine;  // La chaîne claire associée
-} Entry;
+
 
 void sha256_hash_string(const char *string, unsigned char *output) {
     EVP_MD_CTX *mdctx;
@@ -24,58 +22,6 @@ void sha256_hash_string(const char *string, unsigned char *output) {
     EVP_cleanup();
 }
 
-// Fonction pour charger la table de correspondance chaîne-condensat (T3C) depuis un fichier
-int load_table_from_file(const char *filename, Entry **table, int *num_entries) {
-    FILE *file = fopen(filename, "r");
-    if (!file) {
-        perror("Erreur lors de l'ouverture du fichier T3C");
-        return 0;
-    }
-
-    char line[256];
-    int max_entries = 100000000; // Un nombre arbitraire initial de lignes
-    *table = (Entry *)malloc(max_entries * sizeof(Entry));
-    if (*table == NULL) {
-        perror("Erreur lors de l'allocation de mémoire");
-        fclose(file);
-        return 0;
-    }
-
-    *num_entries = 0;
-    while (fgets(line, sizeof(line), file)) {
-        line[strcspn(line, "\n")] = '\0';
-        char *hash = strtok(line, " -> ");
-        char *chaine = strtok(NULL, " -> ");
-
-        if (hash != NULL && chaine != NULL) {
-            (*table)[*num_entries].hash = strdup(hash);
-            (*table)[*num_entries].chaine = strdup(chaine);
-            (*num_entries)++;
-
-            if (*num_entries >= max_entries) {
-                // Réallouer la mémoire pour plus d'entrées si nécessaire
-                max_entries *= 2;
-                *table = (Entry *)realloc(*table, max_entries * sizeof(Entry));
-                if (*table == NULL) {
-                    perror("Erreur lors de la réallocation de mémoire");
-                    fclose(file);
-                    return 0;
-                }
-            }
-        }
-    }
-
-    fclose(file);
-    return 1;
-}
-
-void lookup_in_table(const char *target_hash, Entry *table, int num_entries) {
-    for (int i = 0; i < num_entries; i++) {
-        if (strcmp(target_hash, table[i].hash) == 0) {
-            printf("MATCH %s %s\n", target_hash, table[i].chaine);
-        }
-    }
-}
 
 
 int main(int argc, char *argv[]) {
