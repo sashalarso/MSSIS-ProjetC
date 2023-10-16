712167ad527c1589bf236d357f9216d5834db0e7
sashalarso <85192589+sashalarso@users.noreply.github.com>
2023-10-16 18:22:18 +0200

    Update table.h

--- src/table.h
+++ src/table.h
@@ -7,7 +7,7 @@ typedef struct {
 } Entry;
 
 int load_table_from_file(const char *filename, Entry **table, int *num_entries);
-void lookup_in_table(const char *filename,Entry *table,int num_entries);
+void lookup_in_table(const char *filename,Entry *table,int num_entries,int max_entries);
 
 
 #endif
